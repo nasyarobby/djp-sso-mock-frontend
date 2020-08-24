810daf39abd1c1ae731ab33b6c22d795894fccfd
Nasyarobby <nasyarobby@gmail.com>
2020-08-24 07:40:53 +0700

    initial components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <div
+        className="box rounded shadow-md py-4 px-10 mt-10 mx-auto bg-red-500 text-white invisible"
+        style={{ width: "360px" }}
+      >
+        Login gagal.
+      </div>
+      <div
+        className="box rounded shadow-md py-4 px-10 mt-10 mx-auto bg-green-500 text-white invisible"
+        style={{ width: "360px" }}
+      >
+        Login berhasil.
+      </div>
+      <div
+        className="box rounded-lg shadow-md py-4 px-10 mt-10 mx-auto bg-white"
+        style={{ width: "360px" }}
+      >
+        <h1 className="text-center text-2xl font-semibold">Login</h1>
+        <input
+          placeholder="NPWP"
+          type="text"
+          className="rounded border-gray-400 border p-2 w-full my-2"
+        />
+        <input
+          placeholder="password"
+          type="password"
+          className="rounded border-gray-400 border p-2 w-full my-2"
+        />
+        <button className="bg-orange-400 rounded-full w-full p-2 my-4">
+          Login
+        </button>
+        <div className="text-center">
+          <div>Lupa Kata Sandi ?</div>
+          <div>Belum Registrasi ?</div>
+          <div>Belum Menerima Email Aktivasi ?</div>
+          <div>Atau Belum Punya NPWP ?</div>
+        </div>
+      </div>
     </div>
   );
 }
